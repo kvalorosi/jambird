65fe027756c0793a55ff27e2e9823c710e7318e5
Kaytlin Valorosi <kvalorosi@yahoo.com>
2023-07-30 20:01:46 -0700

    fonts

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,34 +1,33 @@
 import Image from 'react-bootstrap/Image';
 import Carousel from 'react-bootstrap/Carousel';
 import '../views/css/home.css';
+import { Button } from 'react-bootstrap';
 
 
 
 
 const Home = () => {
-   return (
-    <div className="logo">
-        <>
-            <h2><em>Jam Bird is live and ready to go for a ride</em></h2>
-            
+    return (
 
+        <>
+            <div className="logo">
                 <div className="hero-container">
                     <Carousel>
                         <Carousel.Item>
                             <img
-                                className="d-block w-25 m-auto"
+                                className="d-block w-50 m-auto"
                                 src="images/IMG_3170.jpeg"
                                 alt="First slide" />
                         </Carousel.Item>
                         <Carousel.Item>
                             <img
-                                className="d-block w-25 m-auto"
+                                className="d-block w-50 m-auto"
                                 src="images/IMG_7536.jpeg"
                                 alt="Second slide" />
                         </Carousel.Item>
                         <Carousel.Item>
                             <img
-                                className="d-block w-25 m-auto"
+                                className="d-block w-50 m-auto"
                                 src="images/IMG_6012.jpeg"
                                 alt="Third slide" />
                         </Carousel.Item>
@@ -38,12 +37,17 @@ const Home = () => {
                     <div>
 
                     </div>
-                </div>
+                
+            </div>
+            </div>
+            <br />
             
-
-
+            <h2><i>Jam Bird is live and ready to go for a ride!</i></h2>
+            <div className='box'>
+            <Button variant="info">Click Here To Learn More</Button>
+            </div>
         </>
-        </div>
+
 
 
 
--- src/views/Order.jsx
+++ src/views/Order.jsx
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { ListGroup } from "react-bootstrap";
 import Spinner from 'react-bootstrap/Spinner';
+import '../views/css/home.css';
 
 
 
